d3c0ed56d69b7155a1d75f5b673806af3fbf1503
Artyom O <aovsyankin@cognitech.su>
2025-10-14 04:40:38 +0500

    Добавлен чекбокс Рефакторинг renderList()

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,49 @@
 const form = document.getElementById('todo-form') as HTMLFormElement
+const input = document.getElementById('todo-input') as HTMLInputElement
+const todoListHTML = document.getElementById('todo-list') as HTMLElement
 
-const input =document.getElementById('todo-input') as HTMLInputElement
-const list: string[] = []
+type Todo = {
+    text: string;
+    completed: boolean
+}
+
+const list: Todo[] = []
+
 form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(event: Event) {
     event.preventDefault()
-
-    const value:string = input.value
-    console.log(value)
-    list.push(value)
+    const value: string = input.value
+    list.push({ text: value, completed: false })
     input.value = ''
-
-    return list
+    renderList()
 }
 
+function renderList() {
+    todoListHTML.innerHTML = ''
+
+    list.forEach(
+        function (value, index, array) {
+            const divTodoItem = document.createElement('div')
+            const checkbox = document.createElement('input')
+            checkbox.type = 'checkbox'
+            checkbox.checked = value.completed
+
+            checkbox.addEventListener('change', function () {
+                list[index].completed = checkbox.checked
+                renderList()
+            })
+
+            // const textSpan = document.createElement('span')
+
+            divTodoItem.className = 'todo_item'
+            divTodoItem.innerHTML += value.text
+
+            divTodoItem.appendChild(checkbox)
+            todoListHTML.appendChild(divTodoItem)
+        }
+    )
+}
+
+// const li = document.createElement('li')
+//todo вынести переменные кнопок и чекбоксов в отдельные функции, чтобы не было помойки в рендер лист
