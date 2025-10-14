30a9ca06fce906da98c2e66797a56dad8f891310
Artyom O <aovsyankin@cognitech.su>
2025-10-15 04:02:34 +0500

    feat: разделила логику на функции и добавила кнопку удаления задач доб зачеркивание текста

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ const form = document.getElementById('todo-form') as HTMLFormElement
 const input = document.getElementById('todo-input') as HTMLInputElement
 const todoListHTML = document.getElementById('todo-list') as HTMLElement
 
-type Todo = {
+type TodoItem = {
     text: string;
     completed: boolean
 }
 
-const list: Todo[] = []
+const list: TodoItem[] = []
 
 form.addEventListener('submit', handleSubmit)
 
@@ -19,31 +19,62 @@ function handleSubmit(event: Event) {
     renderList()
 }
 
+function createCheckbox(value: TodoItem, index: number) {
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.checked = value.completed
+
+    checkbox.addEventListener('change', function () {
+        list[index].completed = checkbox.checked
+        //todo а если не будет этого элемента по индексу = нужна проверка !
+        renderList()
+    })
+
+    return checkbox
+}
+
+function createTextElement(value: TodoItem) {
+    const textSpan = document.createElement('span')
+    textSpan.textContent = value.text
+
+    if (value.completed) {
+        textSpan.style.textDecoration = 'line-through'
+    }
+
+    return textSpan
+}
+
+function createDeleteButton(index: number) {
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Удалить'
+    deleteButton.addEventListener('click', function () {
+        list.splice(index, 1)
+        renderList()
+    })
+    return deleteButton
+}
+
 function renderList() {
     todoListHTML.innerHTML = ''
 
     list.forEach(
-        function (value, index, array) {
+        function (value, index) {
+            const checkbox = createCheckbox(value, index)
             const divTodoItem = document.createElement('div')
-            const checkbox = document.createElement('input')
-            checkbox.type = 'checkbox'
-            checkbox.checked = value.completed
-
-            checkbox.addEventListener('change', function () {
-                list[index].completed = checkbox.checked
-                renderList()
-            })
-
-            // const textSpan = document.createElement('span')
-
+            const textSpan = createTextElement(value)
+            const deleteButton = createDeleteButton(index)
             divTodoItem.className = 'todo_item'
-            divTodoItem.innerHTML += value.text
+
 
             divTodoItem.appendChild(checkbox)
+            divTodoItem.appendChild(textSpan)
+            divTodoItem.appendChild(deleteButton)
+
             todoListHTML.appendChild(divTodoItem)
         }
     )
 }
 
 // const li = document.createElement('li')
-//todo вынести переменные кнопок и чекбоксов в отдельные функции, чтобы не было помойки в рендер лист
+
+// todo сортировка через фильтр массивов - ис комплитет. 2 варик посмотри sort()
